23ddab6d972e14a47aaec68a186f526d8333d4dc
Austin Jones <oostin623@gmail.com>
2018-09-01 17:01:06 -0400

    add dependencies for angular material

--- foodlog/src/app/app.module.ts
+++ foodlog/src/app/app.module.ts
@@ -3,22 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-//basic persistence for the service
+// basic persistence for the service
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './services/in-memory-data-service';
+import { InMemoryDataService } from './services/in-memory-data-service';
 
+// ng material
+import { MatDialogModule } from '@angular/material';
+
+// foodlog features
 import { AppComponent } from './app.component';
 import { LogFormComponent } from './log-form/log-form.component';
-
-import  { LogDayService } from './services/logDay.service';
-import  { FoodService } from './services/food.service';
-
+import { LogDayService } from './services/logDay.service';
+import { FoodService } from './services/food.service';
 import { FoodDictComponent } from './food-dict/food-dict.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FoodDetailsPipe } from './pipes/food-details.pipe';
-
-
+/* import { AddFoodDialogComponent } from './pipes/food-dict/add-food-dialog/add-food-dialog.component';
+ */
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +31,7 @@ import { FoodDetailsPipe } from './pipes/food-details.pipe';
   ],
   imports: [
     BrowserModule,
+    MatDialogModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
@@ -40,6 +43,7 @@ import { FoodDetailsPipe } from './pipes/food-details.pipe';
     )
   ],
   providers: [LogDayService, FoodService],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [AppComponent]/*,
+   entryConponents: [AddFoodDialogComponent],
+ */})
 export class AppModule { }
